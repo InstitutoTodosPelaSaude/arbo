f6175c71e4b70ba36f98ba518ca4a61cc58735c1
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-11-15 10:58:17 -0300

    FEAT: Add age_groups to combined

--- dbt/models/combined/02_combined_age_groups.sql
+++ dbt/models/combined/02_combined_age_groups.sql
@@ -8,21 +8,9 @@ WITH source_data AS (
     FROM {{ ref("01_combined_join_labs") }}
 
 )
-SELECT
-    *
+SELECT 
+    source_data.*,
+    ag.age_group
 FROM source_data
+LEFT JOIN {{ ref('age_groups') }} AS ag ON source_data.age >= ag." min_age" AND source_data.age <=  ag." max_age"
 
--- age_group
-
--- epiweek
--- month
-
--- country
--- region
--- macroregion
--- macroregion_code
--- state_code
--- state_ibge_code
--- location_ibge_code
--- lat
--- long
