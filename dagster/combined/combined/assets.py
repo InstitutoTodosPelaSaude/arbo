d9a03fc52a4916bcf0f3e3efd1970ec906165ab5
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-11-20 09:10:40 -0300

    FEAT: Export combined to xlsx

--- dagster/combined/combined/assets.py
+++ dagster/combined/combined/assets.py
@@ -1,9 +1,39 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster import AssetExecutionContext, asset
+from dagster_dbt import DbtCliResource, dbt_assets, get_asset_key_for_model
+import pandas as pd
+import os
+import pathlib
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
 from .constants import dbt_manifest_path
 
+load_dotenv()
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 @dbt_assets(manifest=dbt_manifest_path, select='combined')
 def arboviroses_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+@asset(
+    compute_kind="python", 
+    deps=[get_asset_key_for_model([arboviroses_dbt_assets], "04_combined_location")]
+)
+def export_to_xlsx(context):
+    """
+    Get the final combined data from the database and export to xlsx
+    """
+    engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    df = pd.read_sql('select * from arboviroses."04_combined_location"', engine)
+    df.to_excel('data/combined/combined.xlsx', index=False)
+    engine.dispose()
+
+    context.add_output_metadata({
+        'num_rows': df.shape[0],
+        # 'laboratories': df['laboratory'].nunique(),
+    })
+
--- dagster/combined/combined/definitions.py
+++ dagster/combined/combined/definitions.py
@@ -3,12 +3,12 @@ import os
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
-from .assets import arboviroses_dbt_assets
+from .assets import arboviroses_dbt_assets, export_to_xlsx
 from .constants import dbt_project_dir
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[arboviroses_dbt_assets],
+    assets=[arboviroses_dbt_assets, export_to_xlsx],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
