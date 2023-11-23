06e5d5e10109dc3698553b12c9be81abe69a43ea
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-11-23 18:23:35 -0300

    FIX: Select hlagyn columns from raw data

--- dbt/models/hlagyn/hlagyn_01_convert_types.sql
+++ dbt/models/hlagyn/hlagyn_01_convert_types.sql
@@ -9,5 +9,18 @@ WITH source_data AS (
 
 )
 SELECT
-    * -- TODO
+    "Idade"::INT AS age,
+    "Sexo" AS gender,
+    "Pedido" AS test_id,
+    "Data Coleta"::DATE AS date_testing,
+    "Zika vírus" AS zika_result,
+    "Dengue vírus" AS dengue_result,
+    "Chikungunya vírus" AS chikungunya_result,
+    "Tipo Material" AS tipo_material,
+    "Metodologia" AS metodologia, 
+    "Cidade" AS location,
+    "UF" AS state_code,
+    file_name,
+    "Método" AS metodo,
+    "Id cliente" AS id_cliente
 FROM source_data
