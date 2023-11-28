5f76d843f64cc1b1f6e9d69033599fa4ac543e44
João Pedro <joaopedrodasilvalima@gmail.com>
2023-11-28 19:26:08 -0300

    FEAT: HLAGyn fill results, final incremental table

--- dbt/models/hlagyn/hlagyn_03_fill_results.sql
+++ dbt/models/hlagyn/hlagyn_03_fill_results.sql
@@ -16,13 +16,28 @@ SELECT
     date_testing,
     state,
     location,
-    dengue_result AS DENV_test_result,
-    zika_result AS ZIKV_test_result,
-    chikungunya_result AS CHIKV_test_result,
-    NULL AS YFV_test_result,
-    NULL AS MAYV_test_result,
-    NULL AS OROV_test_result,
-    NULL AS WNV_test_result,
+    CASE
+        WHEN dengue_result=1 THEN 'Pos' 
+        WHEN dengue_result=0 THEN 'Neg'
+        WHEN dengue_result=-1 THEN 'NT'
+        ELSE NULL
+    END AS DENV_test_result,
+    CASE
+        WHEN zika_result=1 THEN 'Pos' 
+        WHEN zika_result=0 THEN 'Neg'
+        WHEN zika_result=-1 THEN 'NT'
+        ELSE NULL
+    END AS ZIKV_test_result,
+    CASE
+        WHEN chikungunya_result=1 THEN 'Pos' 
+        WHEN chikungunya_result=0 THEN 'Neg'
+        WHEN chikungunya_result=-1 THEN 'NT'
+        ELSE NULL
+    END AS CHIKV_test_result,
+    'NT' AS YFV_test_result,
+    'NT' AS MAYV_test_result,
+    'NT' AS OROV_test_result,
+    'NT' AS WNV_test_result,
     1 AS qty_original_lines,
     file_name
 
