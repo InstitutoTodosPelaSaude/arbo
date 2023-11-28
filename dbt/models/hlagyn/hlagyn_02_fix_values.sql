44cb82188675453ec7792c79f83f27a6c71eec4a
João Pedro <joaopedrodasilvalima@gmail.com>
2023-11-28 19:10:09 -0300

    FIX: Fix_value state name

--- dbt/models/hlagyn/hlagyn_02_fix_values.sql
+++ dbt/models/hlagyn/hlagyn_02_fix_values.sql
@@ -1,4 +1,35 @@
 {{config(materialized='table')}}
+{%
+    set state_code_to_state_name = {
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+    }
+%}
 
 WITH source_data AS (
     SELECT
@@ -42,11 +73,17 @@ SELECT
         ELSE NULL
     END AS chikungunya_result,
 
-    -- tipo_material,
     location,
-    state_code,
-    
-    COALESCE (metodologia, metodo) AS metodologia,
+    CASE
+        {% for state_code, state_name in state_code_to_state_name.items() %}
+        WHEN state_code = '{{ state_code }}' THEN regexp_replace(upper(unaccent('{{ state_name }}')), '[^\w\s]', '', 'g')
+        {% endfor %}
+        ELSE NULL
+    END AS state,
+
+    -- tipo_material,
+    -- COALESCE (metodologia, metodo) AS metodologia,
+    'arbo_pcr_3' AS test_kit,
     
     file_name
 FROM source_data
