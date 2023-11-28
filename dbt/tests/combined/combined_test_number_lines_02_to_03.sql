32cd5ae8cfa2c258d2aecf85a136844fceac181b
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-11-27 22:28:10 -0300

    FEAT: Add tests for combined

---
+++ dbt/tests/combined/combined_test_number_lines_02_to_03.sql
@@ -0,0 +1,1 @@
+{{ tables_have_the_same_number_lines('combined_02_age_groups', 'combined_03_dates') }}
---
+++ dbt/tests/combined/combined_test_number_lines_03_to_04.sql
@@ -0,0 +1,1 @@
+{{ tables_have_the_same_number_lines('combined_03_dates', 'combined_04_location') }}
