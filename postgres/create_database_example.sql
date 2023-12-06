fac904d0116d40ff4f14d0577683ba42212fcc1a
João Pedro <joaopedrodasilvalima@gmail.com>
2023-12-06 18:22:42 -0300

    FEAT: Dagster write logs to PostgreSQL

--- postgres/create_database_example.sql
+++ postgres/create_database_example.sql
@@ -14,3 +14,15 @@ GRANT ALL PRIVILEGES ON DATABASE itps_dev TO itps_dev;
 CREATE SCHEMA arboviroses;
 GRANT ALL PRIVILEGES ON SCHEMA arboviroses TO itps_dev;
 CREATE EXTENSION unaccent WITH SCHEMA arboviroses;
+
+CREATE DATABASE dagster;
+\c dagster;
+CREATE SCHEMA dagster;
+CREATE USER dagster WITH PASSWORD 'dagster_pass';
+ALTER DATABASE dagster OWNER TO dagster;
+GRANT ALL PRIVILEGES ON DATABASE dagster TO dagster;
+GRANT ALL PRIVILEGES ON SCHEMA public TO dagster;
+GRANT ALL PRIVILEGES ON SCHEMA dagster TO dagster;
+
+ALTER DATABASE dagster SET search_path TO dagster;
+ALTER USER dagster SET search_path TO dagster;
