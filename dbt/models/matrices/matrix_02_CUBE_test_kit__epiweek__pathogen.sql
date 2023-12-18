c2722a39b3d07f4edced168f19c6a9c433c4c250
João Pedro <joaopedrodasilvalima@gmail.com>
2023-12-18 18:43:22 -0300

    FEAT: Calculate KPIs over epiweek, pathogen and test_kit

---
+++ dbt/models/matrices/matrix_02_CUBE_test_kit__epiweek__pathogen.sql
@@ -0,0 +1,33 @@
+
+WITH source_data AS (
+    SELECT
+        *
+    FROM {{ ref("matrix_01_pivoted") }}
+)
+SELECT
+    test_kit,
+    epiweek_enddate,
+    pathogen,
+    
+    -- # Key indicators
+    -- Total Number of Pos
+    -- Total Number of Neg
+    -- Total Number of NT
+    -- Total Number of lines
+    -- Positivity Rate Pos/Pos+Neg
+
+    SUM(CASE WHEN result = 'Pos' THEN 1 ELSE 0 END) AS total_pos,
+    SUM(CASE WHEN result = 'Neg' THEN 1 ELSE 0 END) AS total_neg,
+    SUM(CASE WHEN result = 'NT'  THEN 1 ELSE 0 END) AS total_nt,
+    COUNT(*) AS total_lines,
+    CASE
+        -- Avoid division by zero
+        WHEN SUM(CASE WHEN result IN ('Pos', 'Neg') THEN 1 ELSE 0 END) > 0 THEN
+            SUM(CASE WHEN result = 'Pos' THEN 1 ELSE 0 END) / SUM(CASE WHEN result IN ('Pos', 'Neg') THEN 1 ELSE 0 END)
+        ELSE NULL
+    END AS positivity_rate
+
+FROM
+    source_data
+GROUP BY
+    CUBE(test_kit, epiweek_enddate, pathogen)
