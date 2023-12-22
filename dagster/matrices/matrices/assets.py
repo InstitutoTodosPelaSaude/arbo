38f20bafb7968c47eedaea40d1d869d56bc88ad7
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-12-22 13:18:58 -0300

    FEAT: Create matrix country_posrate_direct_weeks

--- dagster/matrices/matrices/assets.py
+++ dagster/matrices/matrices/assets.py
@@ -25,6 +25,8 @@ dagster_dbt_translator = DagsterDbtTranslator(
     settings=DagsterDbtTranslatorSettings(enable_asset_checks=True)
 )
 
+PATHOGENS = ['DENV']#['CHIKV', 'DENV', 'MAYV', 'OROV', 'WNV', 'YFV', 'ZIKV',]
+
 @dbt_assets(
         manifest=dbt_manifest_path, 
         select='matrices',
@@ -32,4 +34,46 @@ dagster_dbt_translator = DagsterDbtTranslator(
 )
 def arboviroses_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+@asset(
+    compute_kind="python", 
+    deps=[get_asset_key_for_model([arboviroses_dbt_assets], "matrix_02_CUBE_test_kit__epiweek__pathogen")]
+)
+def country_posrate_direct_weeks(context):
+    """
+    Generate matrices from the database and export to tsv
+    """
+    for pathogen in PATHOGENS:
+        # Build query
+        query = f"""
+            SELECT
+                pathogen,
+                epiweek_enddate,
+                positivity_rate
+            FROM arboviroses."matrix_02_CUBE_test_kit__epiweek__pathogen"
+            WHERE
+                pathogen = '{pathogen}' AND
+                test_kit IS NULL AND
+                epiweek_enddate IS NOT NULL
+        """
+
+        # Get results from database
+        engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+        df = pd.read_sql(query, engine)
+
+        # Asserts
+        assert df.epiweek_enddate.nunique() == df.shape[0], 'There are duplicated dates'
+
+        # Transform data to the right format
+        df = df.set_index(['pathogen', 'epiweek_enddate']).unstack('epiweek_enddate').reset_index()
+        new_columns = [(col[0], col[0]) if col[1] == '' else col for col in df.columns.to_list()]
+        df.columns = pd.MultiIndex.from_tuples(new_columns).droplevel(0)
+
+        # Create or manipulate custom columns
+        df.insert(1, f'{pathogen}_test_result', 'Pos')
+
+        # Save in the database
+        df.to_sql(f'matrix_{pathogen}_country_posrate_direct_weeks', engine, schema='arboviroses', if_exists='replace', index=False)
+
+    # engine.dispose()
 
--- dagster/matrices/matrices/definitions.py
+++ dagster/matrices/matrices/definitions.py
@@ -3,12 +3,12 @@ import os
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
-from .assets import arboviroses_dbt_assets
+from .assets import arboviroses_dbt_assets, country_posrate_direct_weeks
 from .constants import dbt_project_dir
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[arboviroses_dbt_assets],
+    assets=[arboviroses_dbt_assets, country_posrate_direct_weeks],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
