97f2213e787399299adfd5ba6a8027f70125060d
João Pedro <joaopedrodasilvalima@gmail.com>
2024-01-09 19:16:55 -0300

    FEAT: Confirm upload

--- streamlit/app/main.py
+++ streamlit/app/main.py
@@ -2,10 +2,26 @@ import streamlit as st
 import os
 
 
-st.title("My first app")
+st.title(":globe_with_meridians: Central ARBO")
 
-uploaded_file = st.file_uploader("Upload a file", type=['csv', 'txt', 'xlsx', 'xls'])
-if uploaded_file is not None:
+st.markdown("## Upload de dados")
+lab = st.selectbox('Laboratório', ['Einstein', 'Hilab', 'HlaGyn'])
+lab = lab.lower()
+
+uploaded_file = st.file_uploader(
+    "Selecione os arquivos", 
+    type=['csv', 'txt', 'xlsx', 'xls', 'tsv']
+)
+if uploaded_file is not None and lab is not None:
     # save uploaded file to disk
-    with open(os.path.join("/app", uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    path = os.path.join("/data", lab)
+
+    st.markdown(
+        f"**Confirmar upload?**\n{uploaded_file.name}"
+    )
+    confirm_bt = st.button("Confirmar")
+    
+    if confirm_bt:
+        st.text("Upload confirmado")
+        with open(os.path.join(path, uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
