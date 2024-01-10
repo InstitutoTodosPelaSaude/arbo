5c2e8a6c510210aac1cf19a7de4458d73945de19
João Pedro <joaopedrodasilvalima@gmail.com>
2024-01-10 17:29:43 -0300

    FIX: Renomear HUB arbo

--- streamlit/app/main.py
+++ streamlit/app/main.py
@@ -4,12 +4,12 @@ import os
 # get env variable 
 DAGSTER_LINK = os.getenv("DAGSTER_LINK")
 
-st.title(":globe_with_meridians: Hub ARBO")
+st.title(":globe_with_meridians: Hub")
 st.divider()
 
 col_1, col_2 = st.columns([0.5, 0.5])
 
-col_1.link_button(":satellite: Central", "central", use_container_width = True)
+col_1.link_button(":satellite: Central Arbo", "central", use_container_width = True)
 # dagster
 col_2.link_button(":octopus: Dagster", DAGSTER_LINK, use_container_width = True)
 # adminer to postgresql
