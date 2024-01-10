0cd9123b9a4d57f13629d7de3846a25380947bf1
João Pedro <joaopedrodasilvalima@gmail.com>
2024-01-10 17:04:01 -0300

    FEAT: Hub arbo + pages

--- streamlit/app/main.py
+++ streamlit/app/main.py
@@ -4,46 +4,19 @@ import os
 LABS = ['Einstein', 'Hilab', 'HlaGyn']
 ACCEPTED_EXTENSIONS = ['csv', 'txt', 'xlsx', 'xls', 'tsv']
 
-st.title(":globe_with_meridians: Central ARBO")
+st.title(":globe_with_meridians: Hub ARBO")
 
-st.markdown("## Upload de dados")
-lab = st.selectbox(
-    'Laboratório', 
-    LABS
-)
-lab = lab.lower()
-
-# Upload file to the server
-# =========================
-uploaded_file = st.file_uploader(
-    "Selecione os arquivos", 
-    type=ACCEPTED_EXTENSIONS
-)
-path = os.path.join("/data", lab)
-
-if uploaded_file is not None and lab is not None:
-    # save uploaded file to disk
-
-    st.markdown(
-        f"**Confirmar upload?**\n{uploaded_file.name}"
-    )
-    confirm_bt = st.button("Confirmar")
-    
-    if confirm_bt:
-        st.text("Upload confirmado")
-        with open(os.path.join(path, uploaded_file.name), "wb") as f:
-            f.write(uploaded_file.getbuffer())
-
+# External Links
+# ==============
 
 st.divider()
-st.markdown("## Explorer\n")
-st.empty()
-for lab_folder in LABS:
-    lab_folder = lab_folder.lower()
-    path = os.path.join("/data", lab_folder)
-    # List files in the folder
-    files = os.listdir(path)
-    files = [ file for file in files if file.endswith(tuple(ACCEPTED_EXTENSIONS)) ]
-    with st.expander(f":file_folder: **{lab_folder}**"):
-        for file in files:
-            st.markdown(f":page_facing_up: {file}")
+
+col_1, col_2, col_3, col_4 = st.columns([0.25, 0.25, 0.25, 0.25])
+
+col_1.link_button(":satellite: Central", "central", use_container_width = True)
+# dagster
+col_2.link_button(":octopus: Dagster", "teste", use_container_width = True)
+# adminer to postgresql
+col_3.link_button(":elephant: Postgres", "google.com", use_container_width = True)
+# DBT docs
+col_4.link_button(":books: DBT Docs", "google.com", use_container_width = True)
