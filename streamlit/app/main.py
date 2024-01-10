60611dbcf12b91a4103caa32b39a4382ba540bc3
João Pedro <joaopedrodasilvalima@gmail.com>
2024-01-10 17:19:14 -0300

    FEAT: Hub as page to dagster and central ARBO

--- streamlit/app/main.py
+++ streamlit/app/main.py
@@ -1,22 +1,18 @@
 import streamlit as st
 import os
 
-LABS = ['Einstein', 'Hilab', 'HlaGyn']
-ACCEPTED_EXTENSIONS = ['csv', 'txt', 'xlsx', 'xls', 'tsv']
+# get env variable 
+DAGSTER_LINK = os.getenv("DAGSTER_LINK")
 
 st.title(":globe_with_meridians: Hub ARBO")
-
-# External Links
-# ==============
-
 st.divider()
 
-col_1, col_2, col_3, col_4 = st.columns([0.25, 0.25, 0.25, 0.25])
+col_1, col_2 = st.columns([0.5, 0.5])
 
 col_1.link_button(":satellite: Central", "central", use_container_width = True)
 # dagster
-col_2.link_button(":octopus: Dagster", "teste", use_container_width = True)
+col_2.link_button(":octopus: Dagster", DAGSTER_LINK, use_container_width = True)
 # adminer to postgresql
-col_3.link_button(":elephant: Postgres", "google.com", use_container_width = True)
+# col_3.link_button(":elephant: Postgres", "google.com", use_container_width = True)
 # DBT docs
-col_4.link_button(":books: DBT Docs", "google.com", use_container_width = True)
+# col_4.link_button(":books: DBT Docs", "google.com", use_container_width = True)
