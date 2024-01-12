cf5cd1984e545dad44adc489b042c325aa509dc8
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-01-12 07:56:30 -0300

    FEAT: Add /dagster and / routes

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # nginx config variable injection
-envsubst < nginx-basic-auth.conf > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
 
 # htpasswd for basic authentication
 htpasswd -c -b /etc/nginx/.htpasswd $BASIC_USERNAME $BASIC_PASSWORD
