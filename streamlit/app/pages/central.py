b151954343aa8e85bcda930b748ca6a44a7792e6
João Pedro <joaopedrodasilvalima@gmail.com>
2024-01-16 17:36:26 -0300

    FEAT: Upload multiple files

--- streamlit/app/pages/central.py
+++ streamlit/app/pages/central.py
@@ -15,41 +15,50 @@ def widgets_list_files_in_folder(path, container):
 
             col_peek, col_download, col_delete = col_buttons.columns( [.3, .3, .3] )
             #col_peek.button(":eye:", key = f"peek_{file}")
-            #col_download.button(":arrow_down:", key = f"download_{file}")
-            #col_delete.button(":x:", key = f"delete_{file}")
+            col_download.button(":arrow_down:", key = f"download_{file}")
+            col_delete.button(":x:", key = f"delete_{file}")
 
     return files
 
+def widgets_upload_file(selected_lab):
+    # Upload file to the server
+    # =========================
+    uploaded_files = st.file_uploader(
+        "Selecione os arquivos", 
+        accept_multiple_files = True,
+        type=ACCEPTED_EXTENSIONS
+    )
+    lab_folder_path = os.path.join("/data", selected_lab)
+
+    if uploaded_files is None or uploaded_files == []:
+        return
+    
+    if selected_lab is None:
+        return
+
+    st.markdown(
+        f"**Confirmar upload?**"
+    )
+    confirm_bt = st.button("Confirmar")
+    if confirm_bt:
+        st.toast("Upload confirmado")
+        for uploaded_file in uploaded_files:
+            with open(os.path.join(lab_folder_path, uploaded_file.name), "wb") as f:
+                f.write(uploaded_file.getbuffer())
 
 st.title(":satellite: Central ARBO")
 
 st.markdown("## Upload de dados")
+
 selected_lab = st.selectbox(
     'Laboratório', 
     LABS
 )
 selected_lab = selected_lab.lower()
+widgets_upload_file(selected_lab)
 
-# Upload file to the server
-# =========================
-uploaded_file = st.file_uploader(
-    "Selecione os arquivos", 
-    type=ACCEPTED_EXTENSIONS
-)
-lab_folder_path = os.path.join("/data", selected_lab)
 
-if uploaded_file is not None and selected_lab is not None:
-    # save uploaded file to disk
 
-    st.markdown(
-        f"**Confirmar upload?**\n{uploaded_file.name}"
-    )
-    confirm_bt = st.button("Confirmar")
-    
-    if confirm_bt:
-        st.text("Upload confirmado")
-        with open(os.path.join(lab_folder_path, uploaded_file.name), "wb") as f:
-            f.write(uploaded_file.getbuffer())
 
 # File Explorer
 # =============
