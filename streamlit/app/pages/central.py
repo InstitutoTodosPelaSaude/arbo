13468a26fe58f736f3d59514d14233df2d69bb5d
João Pedro <joaopedrodasilvalima@gmail.com>
2024-01-16 19:29:46 -0300

    FEAT: Restore files

--- streamlit/app/pages/central.py
+++ streamlit/app/pages/central.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import time
 
 LABS = ['Einstein', 'Hilab', 'HlaGyn']
 ACCEPTED_EXTENSIONS = ['csv', 'txt', 'xlsx', 'xls', 'tsv']
@@ -13,6 +14,14 @@ def delete_file_from_folder(path, filename):
 
     st.toast(f"Arquivo {filename} movido para a lixeira")
 
+def restore_file_from_trash(file_path):
+    # move to _out
+    os.rename(
+        file_path,
+        file_path.replace("_out/", "")
+    )
+
+    st.toast(f"Arquivo {file_path.split('/')[-1]} restaurado")
 
 def widgets_list_files_in_folder(path, container):
     files = os.listdir(path)
@@ -37,6 +46,27 @@ def widgets_list_files_in_folder(path, container):
             )
 
     return files
+
+
+def widgets_list_files_in_folder_checkbox(path, container):
+
+    files = os.listdir(path)
+    files = [ file for file in files if file.endswith(tuple(ACCEPTED_EXTENSIONS)) ]
+
+    files_selected = []
+    with container:
+        
+        for file in files:
+            col_filename, col_checkbox = st.columns([.8, .2])
+
+            col_filename.markdown(f":page_facing_up: {file}")
+            file_is_selected = col_checkbox.checkbox("", key = f"checkbox_{file}")
+
+            if file_is_selected:
+                file_path = os.path.join(path, file)
+                files_selected.append(file_path)
+
+    return files_selected
 
 
 def widgets_upload_file(selected_lab):
@@ -95,3 +125,33 @@ for lab_folder in LABS:
     # List files in the folder
     container = st.expander(f":file_folder: {lab_folder}")
     widgets_list_files_in_folder( lab_folder_path, container )
+
+# Lixeira
+# =======
+    
+st.divider()
+st.markdown("## Lixeira\n")
+st.empty()
+
+files_selected_in_trash = []
+for lab_folder in LABS:
+    lab_folder = lab_folder.lower()
+    lab_folder_path = os.path.join("/data", lab_folder, "_out")
+
+    # List files in the folder
+    container = st.expander(f":file_folder: {lab_folder}")
+    files_selected = widgets_list_files_in_folder_checkbox( lab_folder_path, container )
+    files_selected_in_trash += files_selected
+
+if files_selected_in_trash != []:
+    # Adicionar delete e restore
+    col_button_delete, col_button_restore, _ = st.columns([.15, .15, .7])
+    confirm_bt = col_button_delete.button("Excluir", type='primary')
+    restore_bt = col_button_restore.button("Restaurar")
+
+    if restore_bt:
+        started_restoring_files = True
+        for file in files_selected_in_trash:
+            restore_file_from_trash(file)
+
+        st.rerun()
