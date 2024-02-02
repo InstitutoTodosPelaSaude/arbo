79a3258361f0c6093efd59f2a1094e00c33d780a
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-02-02 19:11:40 -0300

    FEAT: Add sensors and remove used files for Sabin

--- dagster/lab_sabin/lab_sabin/assets.py
+++ dagster/lab_sabin/lab_sabin/assets.py
@@ -95,3 +95,62 @@ def sabin_raw(context):
 )
 def arboviroses_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+@asset(
+    compute_kind="python", 
+    deps=[get_asset_key_for_model([arboviroses_dbt_assets], "sabin_07_final")]
+)
+def sabin_remove_used_files(context):
+    """
+    Remove the files that were used in the dbt process
+    """
+    raw_data_table = 'sabin_raw'
+    files_in_folder = [file for file in os.listdir(SABIN_FILES_FOLDER) if file.endswith(SABIN_CONVERTED_FILES_EXTENSION)]
+
+    # Get the files that were used in the dbt process
+    engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    used_files = pd.read_sql_query(f"SELECT DISTINCT file_name FROM arboviroses.{raw_data_table}", engine).file_name.to_list()
+    engine.dispose()
+
+    # Remove the files that were used
+    path_to_move = SABIN_FILES_FOLDER / "_out"
+    for used_file in used_files:
+        if used_file in files_in_folder:
+            context.log.info(f"Moving file {used_file} to {path_to_move}")
+            shutil.move(SABIN_FILES_FOLDER / used_file, path_to_move / used_file)
+    
+    # Log the unmoved files
+    files_in_folder = os.listdir(SABIN_FILES_FOLDER)
+    context.log.info(f"Files that were not moved: {files_in_folder}")
+
+sabin_all_assets_job = define_asset_job(name="sabin_all_assets_job")
+
+@sensor(
+    job=sabin_all_assets_job,
+    default_status=DefaultSensorStatus.RUNNING
+)
+def new_sabin_file_sensor(context: SensorEvaluationContext):
+    """
+    Check if there are new files in the sabin folder and run the job if there are.
+    The job will only run if the last run is finished to avoid running multiple times.
+    """
+    # Check if there are new files in the sabin folder
+    files = os.listdir(SABIN_FILES_FOLDER)
+    valid_files = [file for file in files if file.endswith(SABIN_RAW_FILES_EXTENSION)]
+    if len(valid_files) == 0:
+        return
+
+    # Get the last run status of the job
+    job_to_look = 'sabin_all_assets_job'
+    last_run = context.instance.get_runs(
+        filters=RunsFilter(job_name=job_to_look)
+    )
+    last_run_status = None
+    if len(last_run) > 0:
+        last_run_status = last_run[0].status
+
+    # If there are no runs running, run the job
+    if last_run_status in FINISHED_STATUSES or last_run_status is None:
+        yield RunRequest()
+    else:
+        yield SkipReason(f"There are files in the sabin folder, but the job {job_to_look} is still running with status {last_run_status}. Files: {valid_files}")
