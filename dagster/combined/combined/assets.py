0fba6e4b09960178bc450c3f9106095b611b9318
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-02-27 23:22:16 -0300

    FEAT: Add slack failure sensor for combined

--- dagster/combined/combined/assets.py
+++ dagster/combined/combined/assets.py
@@ -18,6 +18,7 @@ from dagster_dbt import (
 )
 from dagster.core.storage.pipeline_run import RunsFilter
 from dagster.core.storage.dagster_run import FINISHED_STATUSES
+from dagster_slack import make_slack_on_run_failure_sensor
 import pandas as pd
 import os
 import pathlib
@@ -32,6 +33,8 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+DAGSTER_SLACK_BOT_TOKEN = os.getenv('DAGSTER_SLACK_BOT_TOKEN')
+DAGSTER_SLACK_BOT_CHANNEL = os.getenv('DAGSTER_SLACK_BOT_CHANNEL')
 
 dagster_dbt_translator = DagsterDbtTranslator(
     settings=DagsterDbtTranslatorSettings(enable_asset_checks=True)
@@ -105,3 +108,12 @@ def run_combined_sensor(context: SensorEvaluationContext):
     if any(asset_events.values()):
         context.advance_all_cursors()
         return RunRequest()
+    
+# Failure sensor that sends a message to slack
+combined_slack_failure_sensor = make_slack_on_run_failure_sensor(
+    monitored_jobs=[combined_all_assets_job],
+    slack_token=DAGSTER_SLACK_BOT_TOKEN,
+    channel=DAGSTER_SLACK_BOT_CHANNEL,
+    default_status=DefaultSensorStatus.RUNNING,
+    text_fn = lambda context: f"COMBINED JOB FAILED: {context.failure_event.message}"
+)
