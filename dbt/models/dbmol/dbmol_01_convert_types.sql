5171559f21e5b1ae17fd82a383688ca1dfc098e6
João Pedro <joaopedrodasilvalima@gmail.com>
2024-03-12 18:29:03 -0300

    FEAT: Convert types

--- dbt/models/dbmol/dbmol_01_convert_types.sql
+++ dbt/models/dbmol/dbmol_01_convert_types.sql
@@ -1,1 +1,20 @@
-SELECT 1 AS id
+WITH source_data AS (
+
+    SELECT * FROM
+    {{ source("dagster", "dbmol_raw") }}
+
+)
+SELECT
+    "NumeroPedido" AS test_id,
+    "Sexo" AS sex,
+    -- "idade"::INT AS age,
+    "DataNascimento" AS birth_date,
+    "Procedimento" AS exame,
+    "CodigoProcedimento",
+    "Parametro" AS detalhe_exame,
+    TO_DATE("DataCadastro", 'YYYY-MM-DD') AS date_testing,
+    "Cidade" AS location,
+    "UF" AS state,
+    "Resultado" AS result,
+    file_name
+FROM source_data
