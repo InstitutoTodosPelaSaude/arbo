6832f5d4dc4f3922a981b14469b15ca553e8e7c4
João Pedro <joaopedrodasilvalima@gmail.com>
2024-04-29 20:32:48 -0300

    FIX: Pivot pathogen results, add generic test test_kit_contains_the_correct_pathogen_results

---
+++ dbt/macros/map_sigla_uf_to_name.sql
@@ -0,0 +1,32 @@
+{% macro map_sigla_uf_to_name(column, null_if_not_match=False) %}
+    CASE {{column}}
+        WHEN 'AC' THEN 'ACRE'
+        WHEN 'AL' THEN 'ALAGOAS'
+        WHEN 'AP' THEN 'AMAPA'
+        WHEN 'AM' THEN 'AMAZONAS'
+        WHEN 'BA' THEN 'BAHIA'
+        WHEN 'CE' THEN 'CEARA'
+        WHEN 'DF' THEN 'DISTRITO FEDERAL'
+        WHEN 'ES' THEN 'ESPIRITO SANTO'
+        WHEN 'GO' THEN 'GOIAS'
+        WHEN 'MA' THEN 'MARANHAO'
+        WHEN 'MT' THEN 'MATO GROSSO'
+        WHEN 'MS' THEN 'MATO GROSSO DO SUL'
+        WHEN 'MG' THEN 'MINAS GERAIS'
+        WHEN 'PA' THEN 'PARA'
+        WHEN 'PB' THEN 'PARAIBA'
+        WHEN 'PR' THEN 'PARANA'
+        WHEN 'PE' THEN 'PERNAMBUCO'
+        WHEN 'PI' THEN 'PIAUI'
+        WHEN 'RJ' THEN 'RIO DE JANEIRO'
+        WHEN 'RN' THEN 'RIO GRANDE DO NORTE'
+        WHEN 'RS' THEN 'RIO GRANDE DO SUL'
+        WHEN 'RO' THEN 'RONDONIA'
+        WHEN 'RR' THEN 'RORAIMA'
+        WHEN 'SC' THEN 'SANTA CATARINA'
+        WHEN 'SP' THEN 'SAO PAULO'
+        WHEN 'SE' THEN 'SERGIPE'
+        WHEN 'TO' THEN 'TOCANTINS'
+        ELSE {% if null_if_not_match %} 'NOT_REPORTED' {% else %} {{column}} {% endif %}
+    END
+{% endmacro %}
