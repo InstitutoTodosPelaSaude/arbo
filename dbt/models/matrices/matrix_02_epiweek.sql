7f7a2d46405c8b3405c56d20dcc26abd2c82c823
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-05-20 22:10:43 +0900

    FEAT: New version of matrices by epiweek

---
+++ dbt/models/matrices/matrix_02_epiweek.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    SELECT
+        epiweek_enddate,
+        result,
+        pathogen
+    FROM {{ ref("matrix_01_pivoted") }}
+    WHERE -- FILTER USEFUL TEST KITS FOR EACH PATHOGEN
+        CASE 
+            WHEN "DENV_test_result" IN ('Pos', 'Neg') THEN test_kit IN ('arbo_pcr_3', 'ns1_antigen', 'denv_pcr')
+            WHEN "MAYV_test_result" IN ('Pos', 'Neg') THEN test_kit IN ('mayv_pcr')
+            WHEN "OROV_test_result" IN ('Pos', 'Neg') THEN test_kit IN ('orov_pcr')
+            WHEN "CHIKV_test_result" IN ('Pos', 'Neg') THEN test_kit IN ('chikv_pcr')
+            WHEN "ZIKV_test_result" IN ('Pos', 'Neg') THEN test_kit IN ('zika_pcr')
+            ELSE TRUE
+        END
+)
+SELECT
+    epiweek_enddate,
+    pathogen,
+    {{ matrices_metrics('result') }}
+FROM source_data
+GROUP BY epiweek_enddate, pathogen
+ORDER BY epiweek_enddate, pathogen
