d381fbd6580c57105c57075f4d39d71718a0ad68
patysayonara <patysayonara28@gmail.com>
2024-08-19 15:42:49 -0300

    FIX: Adjust matrix 04 structure

--- dbt/models/matrices/04_DENV_line_posrate_direct_weeks_states.sql
+++ dbt/models/matrices/04_DENV_line_posrate_direct_weeks_states.sql
@@ -6,6 +6,19 @@
 #}
 {% set state_fill_threshold = 0.5 %} 
 
+-- Consulta SQL com Jinja que corresponde ao source_data
+{% set source_data_sql_script %}
+    SELECT
+            epiweek_enddate,
+            state_code,
+            pathogen,
+            {{ matrices_metrics('result') }}
+    FROM {{ ref("matrix_01_pivoted") }}
+    WHERE "DENV_test_result" IN ('Pos', 'Neg') AND test_kit IN ('arbo_pcr_3', 'ns1_antigen', 'denv_pcr')
+    GROUP BY epiweek_enddate, state_code, pathogen
+    ORDER BY epiweek_enddate, state_code, pathogen
+{% endset %}
+
 {% set get_state_codes %}
     SELECT distinct state_code
         FROM (
@@ -14,8 +27,8 @@
                 MAX(total) as total,
                 COUNT(CASE WHEN "posrate" IS NOT NULL AND "posrate" > 0 THEN 1 ELSE NULL END) AS filled
             FROM 
-                {{ ref("matrix_02_epiweek_state") }}, 
-                (SELECT COUNT(DISTINCT epiweek_enddate) as total FROM {{ ref("matrix_02_epiweek_state") }})
+                ({{ source_data_sql_script }}), 
+                (SELECT COUNT(DISTINCT epiweek_enddate) as total FROM ({{ source_data_sql_script }}))
             WHERE pathogen = 'DENV'
             GROUP BY state_code
         ) AS state_fill_rates
@@ -26,14 +39,19 @@
 {% endif %}
 
 WITH source_data AS (
+    {{ source_data_sql_script }}
+),
+
+transformation AS (
     SELECT
         epiweek_enddate,
         state_code,
         MAX(CASE WHEN pathogen = 'DENV' THEN "posrate" * 100 ELSE NULL END) AS "DENV"
-    FROM {{ ref("matrix_02_epiweek_state") }}
+    FROM source_data
     WHERE state_code != 'NOT REPORTED'
     GROUP BY epiweek_enddate, state_code
 )
+
 SELECT
     epiweek_enddate as "Semanas epidemiológicas"
     {% for state in results_list %}
@@ -41,7 +59,7 @@ SELECT
         MAX(CASE WHEN state_code = '{{ state }}' THEN "DENV" ELSE NULL END) AS "{{ state }}"
         {% if not loop.last %},{% endif %}
     {% endfor %}
-FROM source_data
+FROM transformation
 GROUP BY epiweek_enddate
 ORDER BY epiweek_enddate
     
