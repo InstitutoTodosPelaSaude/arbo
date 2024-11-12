9285d4dba2ed0dcc1b160d86c58534b07ec9c5a8
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-11-12 09:57:59 -0300

    FEAT: Load hlagyn files from Minio

--- dagster/lab_hlagyn/lab_hlagyn/assets.py
+++ dagster/lab_hlagyn/lab_hlagyn/assets.py
@@ -21,14 +21,17 @@ from dagster_slack import make_slack_on_run_failure_sensor
 import pandas as pd
 import os
 import pathlib
+import sys
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import shutil
 
 from .constants import dbt_manifest_path
 
-ROOT_PATH = pathlib.Path(__file__).parent.parent.parent.parent.absolute()
-HLAGYN_FILES_FOLDER = ROOT_PATH / "data" / "hlagyn"
+sys.path.insert(1, os.getcwd())
+from filesystem.filesystem import FileSystem
+
+HLAGYN_FILES_FOLDER = "/data/arbo/data/hlagyn/"
 HLAGYN_FILES_EXTENSION = '.xlsx'
 
 load_dotenv()
@@ -49,15 +52,17 @@ def hlagyn_raw(context):
     """
     Table with the raw data from hlagyn
     """
+    file_system = FileSystem(root_path=HLAGYN_FILES_FOLDER)
     engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
-    hlagyn_files = [file for file in os.listdir(HLAGYN_FILES_FOLDER) if file.endswith(HLAGYN_FILES_EXTENSION)]
+    hlagyn_files = [file for file in file_system.list_files_in_relative_path("") if file.endswith(HLAGYN_FILES_EXTENSION)]
     assert len(hlagyn_files) > 0, f"No files found in the folder {HLAGYN_FILES_FOLDER} with extension {HLAGYN_FILES_EXTENSION}"
 
-    hlagyn_df = pd.read_excel(HLAGYN_FILES_FOLDER / hlagyn_files[0], dtype = str)
+    file_to_get = hlagyn_files[0].split("/")[-1] # Get the file name
+    hlagyn_df = pd.read_excel(file_system.get_file_content_as_io_bytes(file_to_get), dtype = str)
     if 'Unnamed: 0' in hlagyn_df.columns: 
         # Some files have an empty row in the beginning
-        hlagyn_df = pd.read_excel(HLAGYN_FILES_FOLDER / hlagyn_files[0], skiprows=1, dtype = str)
+        hlagyn_df = pd.read_excel(file_system.get_file_content_as_io_bytes(file_to_get), skiprows=1, dtype = str)
     hlagyn_df['file_name'] = hlagyn_files[0]
     context.log.info(f"Reading file {hlagyn_files[0]}")
         
@@ -83,7 +88,8 @@ def hlagyn_remove_used_files(context):
     Remove the files that were used in the dbt process
     """
     raw_data_table = 'hlagyn_raw'
-    files_in_folder = [file for file in os.listdir(HLAGYN_FILES_FOLDER) if file.endswith(HLAGYN_FILES_EXTENSION)]
+    file_system = FileSystem(root_path=HLAGYN_FILES_FOLDER)
+    files_in_folder = [file for file in file_system.list_files_in_relative_path("") if file.endswith(HLAGYN_FILES_EXTENSION)]
 
     # Get the files that were used in the dbt process
     engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
@@ -91,14 +97,14 @@ def hlagyn_remove_used_files(context):
     engine.dispose()
 
     # Remove the files that were used
-    path_to_move = HLAGYN_FILES_FOLDER / "_out"
+    path_to_move = "_out/"
     for used_file in used_files:
         if used_file in files_in_folder:
             context.log.info(f"Moving file {used_file} to {path_to_move}")
-            shutil.move(HLAGYN_FILES_FOLDER / used_file, path_to_move / used_file)
+            file_system.move_file_to_folder("", used_file.split("/")[-1], path_to_move)
     
     # Log the unmoved files
-    files_in_folder = os.listdir(HLAGYN_FILES_FOLDER)
+    files_in_folder = file_system.list_files_in_relative_path("")
     context.log.info(f"Files that were not moved: {files_in_folder}")
 
 hlagyn_all_assets_job = define_asset_job(name="hlagyn_all_assets_job")
@@ -113,7 +119,8 @@ def new_hlagyn_file_sensor(context: SensorEvaluationContext):
     The job will only run if the last run is finished to avoid running multiple times.
     """
     # Check if there are new files in the hlagyn folder
-    files = os.listdir(HLAGYN_FILES_FOLDER)
+    file_system = FileSystem(root_path=HLAGYN_FILES_FOLDER)
+    files = file_system.list_files_in_relative_path("")
     valid_files = [file for file in files if file.endswith(HLAGYN_FILES_EXTENSION)]
     if len(valid_files) == 0:
         return
