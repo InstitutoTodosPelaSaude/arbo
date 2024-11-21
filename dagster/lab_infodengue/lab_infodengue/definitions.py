a3407448ff7c621d73d7ea3731c4ccc46079d812
João Pedro <joaopedrodasilvalima@gmail.com>
2024-11-21 20:55:45 -0300

    FEAT: Infodengue remove used files

--- dagster/lab_infodengue/lab_infodengue/definitions.py
+++ dagster/lab_infodengue/lab_infodengue/definitions.py
@@ -3,12 +3,16 @@ import os
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
-from .assets import arboviroses_dbt_assets, infodengue_raw
+from .assets import arboviroses_dbt_assets, infodengue_raw, infodengue_remove_used_files
 from .constants import dbt_project_dir
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[arboviroses_dbt_assets, infodengue_raw],
+    assets=[
+        arboviroses_dbt_assets, 
+        infodengue_raw,
+        infodengue_remove_used_files
+    ],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
