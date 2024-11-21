5d278f74503f891e7d63c83bcb294c9d2d56dff6
João Pedro <joaopedrodasilvalima@gmail.com>
2024-11-21 20:41:28 -0300

    FEAT: Infodengue tests

--- dbt/models/infodengue/infodengue_01_convert_types.sql
+++ dbt/models/infodengue/infodengue_01_convert_types.sql
@@ -1,6 +1,18 @@
 {{ config(materialized='table') }}
 WITH source_data AS (
-    SELECT * FROM
+    SELECT
+        "SE"::TEXT,
+        "casos_est"::INT,
+        "casos_est_min"::INT,
+        "casos_est_max"::INT,
+        "casos"::INT,
+        "state_code",
+        "state",
+        "region",
+        to_date("data_iniSE", 'YYYY-MM-DD') AS "data_iniSE",
+        to_date("data_fimSE", 'YYYY-MM-DD') AS "data_fimSE",
+        "file_name"
+    FROM
     {{ source("dagster", "infodengue_raw") }}
 )
-SELECT 1 AS example
+SELECT * FROM source_data
