b04d5ff073a52d31ac0a49c12ee1ca2d30d9b3b3
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-11-26 13:59:12 -0300

    FEAT: Add matrix 14

---
+++ dbt/models/matrices/14_CHIKV_line_posrate_direct_indirect_week_country_years.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    SELECT
+        CONCAT(
+            'SE', 
+            TO_CHAR(epiweek_number, 'fm00'), 
+            ' - ', 
+            {{ get_month_name_from_epiweek_number('epiweek_number') }}
+        ) as epiweek_month,
+        EXTRACT('Year' FROM epiweek_enddate) as epiweek_year,
+        pathogen,
+        {{ matrices_metrics('result') }}
+    FROM {{ ref("matrix_01_pivoted") }}
+    WHERE 
+        "CHIKV_test_result" IN ('Pos', 'Neg') AND 
+        test_kit IN ('arbo_pcr_3', 'chikv_pcr', 'igm_serum')
+    GROUP BY epiweek_month, epiweek_year, pathogen
+    ORDER BY epiweek_year, epiweek_month, pathogen
+)
+
+SELECT
+    epiweek_month as "year",
+    MAX(CASE WHEN pathogen = 'CHIKV' AND epiweek_year = 2022 THEN "posrate" * 100 ELSE NULL END) as "2022",
+    MAX(CASE WHEN pathogen = 'CHIKV' AND epiweek_year = 2023 THEN "posrate" * 100 ELSE NULL END) as "2023",
+    MAX(CASE WHEN pathogen = 'CHIKV' AND epiweek_year = 2024 THEN "posrate" * 100 ELSE NULL END) as "2024"
+FROM source_data
+GROUP BY epiweek_month
+ORDER BY epiweek_month
+    
