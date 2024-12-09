9a2e52e70ec12663e6fc22d7288962226f606cad
João Pedro <joaopedrodasilvalima@gmail.com>
2024-12-09 00:35:01 -0300

    FEAT: HPardini remove used files and file sensor

--- dagster/lab_hpardini/lab_hpardini/assets.py
+++ dagster/lab_hpardini/lab_hpardini/assets.py
@@ -1,8 +1,14 @@
 from dagster import (
-    AssetExecutionContext,
-    asset,
+    AssetExecutionContext, 
+    asset, 
+    sensor,
+    define_asset_job,
+    RunRequest, 
+    SkipReason, 
+    SensorEvaluationContext,
     MaterializeResult, 
-    MetadataValue
+    MetadataValue,
+    DefaultSensorStatus
 )
 from dagster_dbt import (
     DbtCliResource, 
@@ -11,6 +17,9 @@ from dagster_dbt import (
     DagsterDbtTranslatorSettings,
     get_asset_key_for_model
 )
+from dagster.core.storage.pipeline_run import RunsFilter
+from dagster.core.storage.dagster_run import FINISHED_STATUSES, DagsterRunStatus
+from dagster_slack import make_slack_on_run_failure_sensor
 from textwrap import dedent
 import pandas as pd
 import os
@@ -38,7 +47,8 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_SCHEMA = os.getenv('DB_SCHEMA')
-
+DAGSTER_SLACK_BOT_TOKEN = os.getenv('DAGSTER_SLACK_BOT_TOKEN')
+DAGSTER_SLACK_BOT_CHANNEL = os.getenv('DAGSTER_SLACK_BOT_CHANNEL')
 
 @asset(compute_kind="python")
 def hpardini_raw(context):
@@ -84,3 +94,94 @@ def hpardini_raw(context):
 )
 def arboviroses_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
+
+
+@asset(
+    compute_kind="python", 
+    deps=[get_asset_key_for_model([arboviroses_dbt_assets], "hpardini_final")]
+)
+def hpardini_remove_used_files(context):
+    """
+    Remove the files that were used in the dbt process
+    """
+    raw_data_table = 'hpardini_raw'
+    file_system = FileSystem(root_path=HPARDINI_FILES_FOLDER)
+    files_in_folder = [file for file in file_system.list_files_in_relative_path("") if file.endswith(HPARDINI_FILES_EXTENSION)]
+
+    # Get the files that were used in the dbt process
+    engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    used_files = pd.read_sql_query(f"SELECT DISTINCT file_name FROM arboviroses.{raw_data_table}", engine).file_name.to_list()
+    engine.dispose()
+
+    # Remove the files that were used
+    path_to_move = "_out/"
+    for used_file in used_files:
+        if used_file in files_in_folder:
+            context.log.info(f"Moving file {used_file} to {path_to_move}")
+            file_system.move_file_to_folder("", used_file.split("/")[-1], path_to_move)
+    
+    # Log the unmoved files
+    files_in_folder = file_system.list_files_in_relative_path("")
+    context.log.info(f"Files that were not moved: {files_in_folder}")
+
+hpardini_all_assets_job = define_asset_job(name="hpardini_all_assets_job")
+
+@sensor(
+    job=hpardini_all_assets_job,
+    default_status=DefaultSensorStatus.RUNNING
+)
+def new_hpardini_file_sensor(context: SensorEvaluationContext):
+    """
+    Check if there are new files in the hpardini folder and run the job if there are.
+    The job will only run if the last run is finished to avoid running multiple times.
+    """
+    # Check if there are new files in the hpardini folder
+    file_system = FileSystem(root_path=HPARDINI_FILES_FOLDER)
+    files = file_system.list_files_in_relative_path("")
+    valid_files = [file for file in files if file.endswith(HPARDINI_FILES_EXTENSION)]
+    if len(valid_files) == 0:
+        return
+
+    # Get the last run status of the job
+    job_to_look = 'hpardini_all_assets_job'
+    last_run = context.instance.get_runs(
+        filters=RunsFilter(job_name=job_to_look)
+    )
+    last_run_status = None
+    if len(last_run) > 0:
+        last_run_status = last_run[0].status
+
+    # If there are no runs running, run the job
+    if last_run_status in FINISHED_STATUSES or last_run_status is None:
+        # Do not run if the last status is an error
+        if last_run_status == DagsterRunStatus.FAILURE:
+            return SkipReason(f"Last run status is an error status: {last_run_status}")
+        
+        yield RunRequest()
+    else:
+        yield SkipReason(f"There are files in the hpardini folder, but the job {job_to_look} is still running with status {last_run_status}. Files: {valid_files}")
+
+# Failure sensor that sends a message to slack
+hpardini_slack_failure_sensor = make_slack_on_run_failure_sensor(
+    monitored_jobs=[hpardini_all_assets_job],
+    slack_token=DAGSTER_SLACK_BOT_TOKEN,
+    channel=DAGSTER_SLACK_BOT_CHANNEL,
+    default_status=DefaultSensorStatus.RUNNING,
+    blocks_fn = lambda context: [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"🔴 ARBO: Job '{context.dagster_run.job_name}' failed",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                            "type": "plain_text",
+                            "text": f"{context.failure_event.message}"
+                    }
+                }
+            ]
+)
--- dagster/lab_hpardini/lab_hpardini/definitions.py
+++ dagster/lab_hpardini/lab_hpardini/definitions.py
@@ -3,14 +3,23 @@ import os
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
-from .assets import arboviroses_dbt_assets, hpardini_raw
+from .assets import (
+    arboviroses_dbt_assets, 
+    hpardini_raw,
+    hpardini_remove_used_files,
+    hpardini_all_assets_job,
+    new_hpardini_file_sensor,
+    hpardini_slack_failure_sensor
+)
 from .constants import dbt_project_dir
 from .schedules import schedules
 
 defs = Definitions(
-    assets=[arboviroses_dbt_assets, hpardini_raw],
+    assets=[arboviroses_dbt_assets, hpardini_raw, hpardini_remove_used_files],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
     },
+    jobs=[hpardini_all_assets_job],
+    sensors=[new_hpardini_file_sensor, hpardini_slack_failure_sensor],
 )
