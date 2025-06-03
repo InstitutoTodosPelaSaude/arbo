981d38adfafa85cd498a6bed533760526ed06dcf
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-06-03 13:37:52 -0300

    FIX: Add +test_methods to combined assets

--- dagster/combined/combined/assets.py
+++ dagster/combined/combined/assets.py
@@ -55,7 +55,7 @@ dagster_dbt_translator = DagsterDbtTranslator(
 
 @dbt_assets(
         manifest=dbt_manifest_path, 
-        select='combined +epiweeks +municipios +age_groups +fix_location +fix_state +macroregions',
+        select='combined +epiweeks +municipios +age_groups +fix_location +fix_state +macroregions +test_methods',
         dagster_dbt_translator=dagster_dbt_translator
 )
 def arboviroses_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
