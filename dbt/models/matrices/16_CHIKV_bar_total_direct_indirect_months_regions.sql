c41db4a91ab45cfb814bdb3343941fe4bbdd4c25
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-06-04 13:44:28 -0300

    FEAT: Create month version from matrix 16

---
+++ dbt/models/matrices/16_CHIKV_bar_total_direct_indirect_months_regions.sql
@@ -0,0 +1,38 @@
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    SELECT
+        "month",
+        region,
+        pathogen,
+        {{ matrices_metrics('result') }}
+    FROM {{ ref("matrix_01_pivoted") }}
+    WHERE 
+        "CHIKV_test_result" IN ('Pos', 'Neg') AND 
+        test_kit IN ('arbo_pcr_3', 'igm_serum', 'chikv_pcr') AND 
+        region != 'NOT REPORTED'
+    GROUP BY "month", region, pathogen
+    ORDER BY "month", region, pathogen
+),
+
+transformation AS (
+    SELECT
+        "month",
+        region,
+        SUM(CASE WHEN pathogen = 'CHIKV' THEN totaltests ELSE 0 END) AS "CHIKV"
+    FROM source_data
+    GROUP BY "month", region
+    ORDER BY "month", region
+)
+
+SELECT
+    "month" as "Ano-Mês",
+    MAX(CASE WHEN region = 'Centro-Oeste' THEN "CHIKV" ELSE 0 END) as "Centro-Oeste",
+    MAX(CASE WHEN region = 'Nordeste' THEN "CHIKV" ELSE 0 END) as "Nordeste",
+    MAX(CASE WHEN region = 'Norte' THEN "CHIKV" ELSE 0 END) as "Norte",
+    MAX(CASE WHEN region = 'Sudeste' THEN "CHIKV" ELSE 0 END) as "Sudeste",
+    MAX(CASE WHEN region = 'Sul' THEN "CHIKV" ELSE 0 END) as "Sul"
+FROM transformation
+GROUP BY "month"
+ORDER BY "month"
+    
