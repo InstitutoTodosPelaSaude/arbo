c0cff27e2505339c78e83029d35cc49d71118e00
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2026-03-05 17:48:37 +0000

    FIX: Update startdate

--- dbt/models/matrices/12_CHIKV_map_pos_direct_cities_h.sql
+++ dbt/models/matrices/12_CHIKV_map_pos_direct_cities_h.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
-{% set epiweek_start = '2024-11-24' %}
+{% set epiweek_start = '2026-02-01' %}
 
 -- CTE para selecionar todas as datas finais de semana epidemiológica
 WITH epiweeks AS (
