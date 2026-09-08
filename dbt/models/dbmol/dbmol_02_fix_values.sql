72c6ac0025d7242c1b439e9b210d07487687373a
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2026-09-08 20:15:22 +0000

    add_changes_in_02_fix_values

--- dbt/models/dbmol/dbmol_02_fix_values.sql
+++ dbt/models/dbmol/dbmol_02_fix_values.sql
@@ -69,6 +69,16 @@ source_data_fix_values AS (
                     WHEN detalhe_exame = 'AACHIM' THEN 'igm_serum'
                     ELSE 'UNKNOWN'
                 END
+            --- Acrescimo de novos exames
+            WHEN 'MONKEYPOX' THEN 'mpox_pcr'
+	        WHEN 'FEBRE AMARELA - DETECÇÃO POR PCR' THEN 'feama_pcr'
+	        WHEN 'ESTUDO SOROLÓGICO -  VÍRUS FEBRE AMARELA IGG E IGM'
+	        THEN 
+		        CASE 
+	 	            WHEN detalhe_exame = 'IGG' THEN 'feama_igg'
+                    WHEN detalhe_exame = 'IGM' THEN 'feama_igm'
+                    ELSE 'UNKNOWN' 
+                END
 
             ELSE 'UNKNOWN'
         END AS test_kit,
@@ -111,13 +121,16 @@ source_data_fix_values AS (
 
             -- Testes diversos com resultado textual
             WHEN 
-                detalhe_exame IN ('DNS1', 'ZIKA', 'TDENGE', 'CHIKV', 'ZIKAP')
+                detalhe_exame IN ('DNS1', 'ZIKA', 'TDENGE', 'CHIKV', 'ZIKAP', 'FEAMA', 'MPOX')
                 OR
                 ( exame ILIKE 'PAINEL DE ARBOVIROSES%' AND detalhe_exame IN ('CHIKUN', 'DENGUE', 'ZIKAV') ) -- PZDC
+                OR
+                (exame = 'ESTUDO SOROLÓGICO -  VÍRUS FEBRE AMARELA IGG E IGM' AND detalhe_exame IN ('IGG', 'IGM'))
             THEN
                 CASE 
-                    WHEN result IN ('POSITIVO', 'POSITIVA', 'DETECTADO')     THEN 1
-                    WHEN result IN ('NEGATIVO', 'NEGATIVA', 'NAO DETECTADO') THEN 0
+                    WHEN result IN ('POSITIVO', 'POSITIVA', 'DETECTADO')     
+                        OR result ILIKE 'REAGENTE%' THEN 1
+                    WHEN result IN ('NEGATIVO', 'NEGATIVA', 'NAO DETECTADO', 'NAO REAGENTE') THEN 0
                     ELSE {{ NAO_RECONHECIDO }}
                 END
 
@@ -137,22 +150,186 @@ source_data_fix_values AS (
 
     FROM source_data
     WHERE 1=1
-    AND "CodigoProcedimento" IN
+    AND "CodigoProcedimento" NOT IN
     (
-        'DENGM',
-        'DENGG',
-        'AACHI',
-        'ZICAM',
-        'ZICAG',
-        'DNS1',
-        'CHIKUNM',
-        'CHIKUNG',
-        'PZDC',
-        'ZIKAP',
-        'CHIKV',
-        'MAYA',
-        'TDENGE',
-        'ZIKA'
+    'AALM1',
+    'AAU',
+    'AAYE',
+    'ACIMR',
+    'ACIMR2',
+    'ADENF',
+    'ADENO',
+    'AGLM1',
+    'AGLM4',
+    'AHBE',
+    'AHCV',
+    'ALM4B',
+    'ANSP',
+    'B19PC',
+    'BORPC',
+    'CCARB',
+    'CESBL',
+    'CHLAI',
+    'CHLGT',
+    'CHLMT',
+    'CHLTM',
+    'CLHGE',
+    'CMGL',
+    'CMPCR',
+    'CMVAV',
+    'CMVG',
+    'CMVM',
+    'CMVQT',
+    'COV19A,'
+    'COVI19Q',
+    'CRYPC',
+    'CRYUM',
+    'CTAG',
+    'CTCH',
+    'CTNG',
+    'CTPCR',
+    'CUBAR',
+    'CUL2',
+    'CULANA,'
+    'CULB',
+    'CULBA',
+    'CULE',
+    'CULEFC',
+    'CULES',
+    'CULF',
+    'CULF10',
+    'CULF2',
+    'CULF3',
+    'CULF4',
+    'CULF5',
+    'CULF6',
+    'CULF7',
+    'CULF8',
+    'CULF9',
+    'CULFS',
+    'CULFU',
+    'CULH',
+    'CULIQ',
+    'CULLA',
+    'CULLP',
+    'CULLS',
+    'CULM',
+    'CULMU',
+    'CULN',
+    'CULNE',
+    'CULNF',
+    'CULNV',
+    'CULOS',
+    'CULOU',
+    'CULPC',
+    'CULPE',
+    'CULPR',
+    'CULS2',
+    'CULSB',
+    'CULSO',
+    'CULST',
+    'CULT',
+    'CULU',
+    'CULUR',
+    'CULV',
+    'CUVG',
+    'CVRE',
+    'DNADE',
+    'EHECPC',
+    'FLUAB',
+    'FTAG',
+    'FTAGL',
+    'HBCG',
+    'HBCM',
+    'HBEAG',
+    'HBPCR',
+    'HBQL',
+    'HBQT',
+    'HCVGE',
+    'HCVPC',
+    'HCVQF',
+    'HCVQT',
+    'HCVRI',
+    'HEMA1',
+    'HEMC',
+    'HEMOC',
+    'HEPD',
+    'HEPDAG',
+    'HEPDM',
+    'HERG',
+    'HERGL',
+    'HERM',
+    'HERP',
+    'HERPS',
+    'HFUN',
+    'HIVPC',
+    'HIVQT',
+    'HIVWB',
+    'HMA1',
+    'HPVAB',
+    'HPVABP',
+    'HPVC',
+    'HPVG',
+    'HPVRT',
+    'HTLV',
+    'HTLVGE,'
+    'HTLVI',
+    'HTLVSE',
+    'HTLVW',
+    'IGGCM',
+    'IGGRB',
+    'IGMRB',
+    'INFAG',
+    'LEGIO',
+    'LEGPA',
+    'LEGPG',
+    'LEGPM',
+    'LEGPN',
+    'LISPCR',
+    'MENLX',
+    'MYPNA',
+    'MYPNE',
+    'MYPNG',
+    'MYPNM',
+    'NCMVM',
+    'NEUCOV',
+    'NGCH',
+    'NGPCR',
+    'NOROV',
+    'NRUBM',
+    'PB19G',
+    'PB19I',
+    'PB19M',
+    'PCRYP',
+    'PCRYS',
+    'PMDD',
+    'PNEUPC',
+    'POSHEMO',
+    'POSURI',
+    'PSEMR',
+    'PVIROC',
+    'PVIROL',
+    'RESP4',
+    'ROTA',
+    'RUBAV',
+    'RUBE',
+    'RUBEG',
+    'RUBEM',
+    'SALG',
+    'SALM',
+    'SARAG',
+    'SARAM',
+    'SIFT',
+    'SINRE',
+    'STREAG',
+    'STREP',
+    'STREPT',
+    'TREPCR',
+    'TSMIC',
+    'UREAG',
+    'UREAM',
+    'USONDA',
+    'UUPCR'
     )
     AND NOT detalhe_exame IN (
         'MAT', 'MATERIAL', 'METODO', 'SOROTI',
